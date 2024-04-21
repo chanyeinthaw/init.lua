916ccfa9d654f5ad55851d9662ee992d0683630d
Chan Nyein Thaw <me@chanyeinthaw.com>
2024-04-21 20:07:24 +0700

    supress ~/, ~/Downloads and /

--- lua/cnt/lazy/autosession.lua
+++ lua/cnt/lazy/autosession.lua
@@ -4,7 +4,7 @@ return {
 		config = function()
 			require("auto-session").setup({
 				log_level = "error",
-				-- auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+				auto_session_suppress_dirs = { "~/", "~/Downloads", "/" },
 				-- cwd_change_handling = {
 				-- 	post_cwd_changed_hook = function()
 				-- 		-- require("lualine").refresh()
