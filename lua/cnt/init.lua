abb7af3dcb6486405897319331c322e7416914dd
Chan Nyein Thaw <me@chanyeinthaw.com>
2024-04-21 17:55:34 +0700

    restructure

---
+++ lua/cnt/init.lua
@@ -0,0 +1,7 @@
+require("cnt.settings")
+require("cnt.remap")
+require("cnt.autocmd")
+require("cnt.lazy_init")
+require("cnt.plugin_config")
+
+-- vim: ts=2 sts=2 sw=2 et
---
+++ lua/cnt/plugin_config.lua
@@ -0,0 +1,2 @@
+-- require("leap").create_default_mappings()
+-- vim: ts=2 sts=2 sw=2 et
