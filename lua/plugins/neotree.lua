5719ac121098f5cdc74c7cac240be1ea8a8156eb
Chan Nyein Thaw <me@chanyeinthaw.com>
2024-04-21 15:21:11 +0700

    CNT Neovim

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,42 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	cmd = "Neotree",
+	keys = {
+		{ "\\", ":Neotree reveal current<CR>", { desc = "NeoTree reveal" } },
+		{
+			"<leader>b",
+			":Neotree buffers current<CR>",
+			{ desc = "NeoTree buffers" },
+		},
+	},
+	opts = {
+		filesystem = {
+			window = {
+				position = "right",
+				mappings = {
+					["\\"] = "close_window",
+				},
+			},
+		},
+
+		buffers = {
+			window = {
+				position = "right",
+				mappings = {
+					["\\"] = "close_window",
+				},
+			},
+		},
+	},
+}
+
+-- vim: ts=2 sts=2 sw=2
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		opts = {
+			open_mapping = [[<C-\>]],
+		},
+	},
+}
+-- vim: ts=2 sts=2 sw=2
