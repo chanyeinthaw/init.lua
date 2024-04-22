a4d68b1502880a2d7b15a4e2e24bb25aa0bdc7f0
Chan Nyein Thaw <me@chanyeinthaw.com>
2024-04-22 15:32:44 +0700

    disable emmet

--- lua/cnt/lazy/lsp.lua
+++ lua/cnt/lazy/lsp.lua
@@ -160,31 +160,31 @@ return {
 				-- But for many setups, the LSP (`tsserver`) will work just fine
 				-- tsserver = {},
 				--
-				emmet_ls = {
-					capabilities = capabilities,
-					filetypes = {
-						"css",
-						"eruby",
-						"html",
-						"javascript",
-						"javascriptreact",
-						"less",
-						"sass",
-						"scss",
-						"svelte",
-						"pug",
-						"typescriptreact",
-						"vue",
-					},
-					init_options = {
-						html = {
-							options = {
-								-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-								["bem.enabled"] = true,
-							},
-						},
-					},
-				},
+				-- emmet_ls = {
+				-- 	capabilities = capabilities,
+				-- 	filetypes = {
+				-- 		"css",
+				-- 		"eruby",
+				-- 		"html",
+				-- 		"javascript",
+				-- 		"javascriptreact",
+				-- 		"less",
+				-- 		"sass",
+				-- 		"scss",
+				-- 		"svelte",
+				-- 		"pug",
+				-- 		"typescriptreact",
+				-- 		"vue",
+				-- 	},
+				-- 	init_options = {
+				-- 		html = {
+				-- 			options = {
+				-- 				-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+				-- 				["bem.enabled"] = true,
+				-- 			},
+				-- 		},
+				-- 	},
+				-- },
 
 				lua_ls = {
 					-- cmd = {...},
