da208e268a4e732f8d01e58271925f582c55ad1c
Chan Nyein Thaw <me@chanyeinthaw.com>
2024-04-24 22:24:10 +0700

    change some telescoope keymaps

--- lua/cnt/lazy/telescope.lua
+++ lua/cnt/lazy/telescope.lua
@@ -85,13 +85,13 @@ return {
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 
 			-- vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-			vim.keymap.set("n", "<leader><leader>", function()
+			vim.keymap.set("n", "<C-S-E>", function()
 				builtin.buffers()
 				-- send_escape()
 			end, { desc = "[ ] Find existing buffers" })
 
 			-- Slightly advanced example of overriding default behavior and theme
-			vim.keymap.set("n", "<leader>/", function()
+			vim.keymap.set("n", "/", function()
 				-- You can pass additional configuration to Telescope to change the theme, layout, etc.
 				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
 					winblend = 10,
