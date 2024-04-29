f015c9e2f1348923dfb2adb82931f795f39fbd54
Chan Nyein Thaw <me@chanyeinthaw.com>
2024-04-29 18:05:26 +0700

    add minimap & diagnostic tool

---
+++ lua/cnt/lazy/minimap.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"gorbit99/codewindow.nvim",
+		config = function()
+			local codewindow = require("codewindow")
+			codewindow.setup()
+			codewindow.apply_default_keybinds()
+		end,
+	},
+}
+-- vim: ts=2 sts=2 sw=2
