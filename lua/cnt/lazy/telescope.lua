cc251ed6d091cebfa21e7943ec67c485dfafdaee
Chan Nyein Thaw <me@chanyeinthaw.com>
2024-04-29 13:13:47 +0700

    search buffers keybind and ui change

--- lua/cnt/lazy/telescope.lua
+++ lua/cnt/lazy/telescope.lua
@@ -85,10 +85,13 @@ return {
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 
 			-- vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-			vim.keymap.set("n", "<C-S-E>", function()
-				builtin.buffers()
+			vim.keymap.set("n","<leader>sb", function()
+				builtin.buffers(require("telescope.themes").get_dropdown({
+					winblend = 10,
+					previewer = false,
+				}))
 				-- send_escape()
-			end, { desc = "[ ] Find existing buffers" })
+			end, { desc = "[S]earch [b]uffers" })
 
 			-- Slightly advanced example of overriding default behavior and theme
 			vim.keymap.set("n", "/", function()
