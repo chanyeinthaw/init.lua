c3e0b06b7403d3343bce3343a26588fbbbb4a111
Chan Nyein Thaw <me@chanyeinthaw.com>
2024-05-04 13:26:40 +0700

    disable toggleterm

--- lua/cnt/lazy/toggleterm.lua
+++ lua/cnt/lazy/toggleterm.lua
@@ -1,11 +1,11 @@
 return {
-	{
-		"akinsho/toggleterm.nvim",
-		version = "*",
-		opts = {
-			open_mapping = [[<C-\>]],
-			size = 25,
-		},
-	},
+	-- {
+	-- 	"akinsho/toggleterm.nvim",
+	-- 	version = "*",
+	-- 	opts = {
+	-- 		open_mapping = [[<C-\>]],
+	-- 		size = 25,
+	-- 	},
+	-- },
 }
 -- vim: ts=2 sts=2 sw=2
