4f2218a68648791119570a6bbaae56e1cb8e8488
Chan Nyein Thaw <me@chanyeinthaw.com>
2024-06-08 16:10:22 +0700

    disable auto-session & session lens

--- lua/cnt/lazy/autosession.lua
+++ lua/cnt/lazy/autosession.lua
@@ -1,35 +1,35 @@
 return {
-	{
-		"rmagatti/auto-session",
-		config = function()
-			require("auto-session").setup({
-				log_level = "error",
-				auto_session_suppress_dirs = { "~/", "~/Downloads", "/" },
-				-- cwd_change_handling = {
-				-- 	post_cwd_changed_hook = function()
-				-- 		-- require("lualine").refresh()
-				-- 	end,
-				-- },
-			})
-		end,
-	},
-	{
-		"rmagatti/session-lens",
-		requires = { "rmagatti/auto-session", "nvim-telescope/telescope.nvim" },
-		config = function()
-			require("session-lens").setup({
-				prompt_title = "Sessions",
-			})
-
-			-- vim.keymap.set("n", "<leader>ss", "<cmd>SearchSession<CR>", {
-			-- 	desc = "[S]earch [s]essions",
-			-- })
-			vim.keymap.set("n", "<leader>ss", function()
-				vim.cmd("SearchSession")
-				-- _G.send_escape()
-			end, {
-				desc = "[S]earch [s]essions",
-			})
-		end,
-	},
+	-- {
+	-- 	"rmagatti/auto-session",
+	-- 	config = function()
+	-- 		require("auto-session").setup({
+	-- 			log_level = "error",
+	-- 			auto_session_suppress_dirs = { "~/", "~/Downloads", "/" },
+	-- 			-- cwd_change_handling = {
+	-- 			-- 	post_cwd_changed_hook = function()
+	-- 			-- 		-- require("lualine").refresh()
+	-- 			-- 	end,
+	-- 			-- },
+	-- 		})
+	-- 	end,
+	-- },
+	-- {
+	-- 	"rmagatti/session-lens",
+	-- 	requires = { "rmagatti/auto-session", "nvim-telescope/telescope.nvim" },
+	-- 	config = function()
+	-- 		require("session-lens").setup({
+	-- 			prompt_title = "Sessions",
+	-- 		})
+	--
+	-- 		-- vim.keymap.set("n", "<leader>ss", "<cmd>SearchSession<CR>", {
+	-- 		-- 	desc = "[S]earch [s]essions",
+	-- 		-- })
+	-- 		vim.keymap.set("n", "<leader>ss", function()
+	-- 			vim.cmd("SearchSession")
+	-- 			-- _G.send_escape()
+	-- 		end, {
+	-- 			desc = "[S]earch [s]essions",
+	-- 		})
+	-- 	end,
+	-- },
 }
