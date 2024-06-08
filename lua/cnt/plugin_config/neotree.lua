faa11ff89a6456839d32bbcdfde7513bfc5c1039
Chan Nyein Thaw <me@chanyeinthaw.com>
2024-06-08 16:12:59 +0700

    neotree float title transparent & re-enable auto-session

--- lua/cnt/plugin_config/neotree.lua
+++ lua/cnt/plugin_config/neotree.lua
@@ -3,5 +3,6 @@ vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
 
 local bg = current_bg_color()
 if bg then
-  vim.api.nvim_set_hl(0, "NeoTreeFloatBorder", {bg=bg})
+  vim.api.nvim_set_hl(0, "NeoTreeFloatTitle", { bg = bg })
+  vim.api.nvim_set_hl(0, "NeoTreeFloatBorder", { bg = bg })
 end
