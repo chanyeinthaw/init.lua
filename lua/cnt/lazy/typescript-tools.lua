954865503275238b30b3dc99c741739cd6717e1c
Chan Nyein Thaw <me@chanyeinthaw.com>
2025-01-24 15:37:24 +0900

    read typescript import module specifier pref from .nvim.json

--- lua/cnt/lazy/typescript-tools.lua
+++ lua/cnt/lazy/typescript-tools.lua
@@ -1,3 +1,19 @@
+local function get_import_module_pref()
+  local root_dir = vim.fn.getcwd()
+  local nvim_cfg = root_dir .. "/.nvim.json"
+  if not vim.loop.fs_stat(nvim_cfg) then
+    return "non-relative"
+  end
+
+  nvim_cfg = vim.fn.json_decode(vim.fn.readfile(nvim_cfg))
+
+  if nvim_cfg.typescript_importModuleSpecifierPreference == nil then
+    return "non-relative"
+  end
+
+  return nvim_cfg.typescript_importModuleSpecifierPreference
+end
+
 return {
   {
     "pmizio/typescript-tools.nvim",
@@ -9,7 +25,10 @@ return {
           filetypes = { "javascriptreact", "typescriptreact" },
         },
         tsserver_file_preferences = {
-          importModuleSpecifierPreference = "non-relative",
+          -- non-relative = always absolute
+          -- relative = relative to the project root
+          -- project-relative = ???
+          importModuleSpecifierPreference = get_import_module_pref(),
         },
       },
     },
