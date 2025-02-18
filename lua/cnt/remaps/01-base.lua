58f75a8ddab951dc0b83130d8794d9dfb7a59e08
Chan Nyein Thaw <me@chanyeinthaw.com>
2025-02-18 13:14:43 +0900

    make center cursor after c-d, c-u and s-g

--- lua/cnt/remaps/01-base.lua
+++ lua/cnt/remaps/01-base.lua
@@ -45,3 +45,8 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Ctrl-d Ctrl-u center
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll [D]own" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll [D]own" })
+vim.keymap.set("n", "<S-g>", "<S-g>zz", { desc = "Scroll end" })
