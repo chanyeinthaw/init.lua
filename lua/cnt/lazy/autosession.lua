a5e653a362769a1d2e88bf408fd24b45aa08b8b9
Chan Nyein Thaw <me@chanyeinthaw.com>
2025-05-08 13:57:33 +0900

    switch command in nvim

--- lua/cnt/lazy/autosession.lua
+++ lua/cnt/lazy/autosession.lua
@@ -5,6 +5,7 @@ return {
       require("auto-session").setup({
         log_level = "error",
         auto_session_suppress_dirs = { "~/", "~/Downloads", "/" },
+
         -- cwd_change_handling = {
         -- 	post_cwd_changed_hook = function()
         -- 		-- require("lualine").refresh()
